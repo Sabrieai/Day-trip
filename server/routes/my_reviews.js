f7353e2e495507f60f65b4d2df45cebe61532b59
Aisha Sabrie <sabriealt@gmail.com>
2022-02-15 16:34:38 -0500

    Add get and post requests to /myreviews

--- server/routes/my_reviews.js
+++ server/routes/my_reviews.js
@@ -1,5 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const reviews = require('../db/reviews_queries');
 
-// get to /id to get reviews
-// post reviews form
+//redirect to get reviews for user
+router.get('/', (req, res) => {
+  const id = req.session;
+  res.redirect(`/${id}`);
+});
+
+// get reviews for a user
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  reviews.getMyReviews(id)
+    .then((reviews) => {
+      res.json(reviews);
+    });
+});
+
+// posts after writing my review
+router.post('/:id/:resId/', (req, res) => {
+  const adventure = req.params.id;
+  const guest = req.session;
+  const reservation = req.params.resId;
+  const rating = req.params.rating;
+  const comment = req.params.comment;
+  reviews.addReview(guest, adventure, reservation, rating, comment)
+    .then((reviews) => {
+      res.json(reviews);
+    });
+});

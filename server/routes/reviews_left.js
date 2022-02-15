a02b324b76667cc5f23bbd21404eec95c3be7ac5
Aisha Sabrie <sabriealt@gmail.com>
2022-02-15 16:41:07 -0500

    Add route to get my activities reviews

--- server/routes/reviews_left.js
+++ server/routes/reviews_left.js
@@ -1,3 +1,17 @@
 const express = require("express");
 const router = express.Router();
-//get reviews left 
+const reviews = require('../db/reviews_queries');
+
+
+router.get('/', (req, res) => {
+  const id = req.session;
+  res.redirect(`/${id}`);
+});
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  reviews.getMyAdventureReviews(id)
+    .then((reviews) => {
+      res.json(reviews);
+    });
+});

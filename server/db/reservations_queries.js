2c2cca91a91626b4ccdfd01960b54e4b6da04a68
Aisha Sabrie <sabriealt@gmail.com>
2022-02-15 15:10:27 -0500

    make reservations queries dynamic

--- server/db/reservations_queries.js
+++ server/db/reservations_queries.js
@@ -1,21 +1,21 @@
 const { pool } = require('./database');
 
 
-const getReservations = () => {
+const getReservations = (id) => {
   // const guest = req.session.user_id
   return pool.query(`SELECT title, city, thumbnail_photo_url, reservations.date FROM adventures 
   JOIN reservations 
   ON adventures.id = adventure_id
   WHERE guest_id = $1
-  `, [20])
+  `, [id])
     .then((response) => {
       return response.rows;
     })
     .catch((err) => {
       return err.message;
     });
-}
+};
 
 
 
-module.exports = { getReservations }
+module.exports = { getReservations };

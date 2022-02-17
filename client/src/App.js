39c1060297e0f658af60cdb1eee6321f4d7a3890
mpham-12 <martinp0512@gmail.com>
2022-02-17 17:09:08 -0500

    create router pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 // import { GoogleLogin } from 'react-google-login';
 import ReviewForm from './components/ReviewForm'
 import AdventureForm from './components/AdventureForm';
@@ -7,7 +9,7 @@ import MyAdventures from './components/MyAdventures';
 import ReservationForm from './components/ReservationForm';
 import ToggleFavourite from "./components/ToggleFavourite";
 import SearchBar from './components/SearchBar';
-
+import Home from './Pages/Home';
 
 // const responseGoogle = response => {
 //   console.log(response);
@@ -15,19 +17,31 @@ import SearchBar from './components/SearchBar';
 
 const App = () => {
   return (
-    <div className='App'>
-      <ReviewForm />
-      <AdventureForm />
-      <MyAdventures />
-      <ReservationForm />
-      {/* <GoogleLogin
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+
+        {/* <div className='App'> */}
+        {/* <ReviewForm />
+          <AdventureForm />
+          <MyAdventures />
+          <ReservationForm /> */}
+        {/* <GoogleLogin
         clientId='808382542479-3st5n36i6eamdtgu9bqghm4811s4aree.apps.googleusercontent.com'
         onSuccess={responseGoogle}
         onFailure={responseGoogle}
       /> */}
-      <ToggleFavourite/>
-      <SearchBar/>
-    </div>
+        {/* <ToggleFavourite />
+          <SearchBar /> */}
+        {/* </div> */}
+
+      </Routes>
+    </Router>
   );
 
 };

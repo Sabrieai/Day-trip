4e79d49ff87b530d391d612026d8e288888896db
“Victoria <victoriahall.dev@gmail.com>
2022-02-20 21:03:19 +0000

    styled adventure details page

--- client/src/components/Bookings.js
+++ client/src/components/Bookings.js
@@ -1,6 +1,13 @@
 import React, { useContext, useEffect, useState } from "react"
 import { userContext } from "../providers/UserProvider"
 import useApplicationData from "../hooks/useApplicationData"
+import { FaCalendarAlt } from "react-icons/fa";
+import { FaUserPlus } from "react-icons/fa";
+import { FaDollarSign } from "react-icons/fa";
+import { FaCalculator } from "react-icons/fa";
+
+import './Bookings.css'
+
 
 export default function Bookings(props) {
     const schedule = props.schedule
@@ -30,22 +37,29 @@ useEffect(() => {
     setTotal(guests * price)
 }, [guests])
 return (
-    <>
-        <h1> {day.day} </h1>
-        <span> {price} / pp </span>
-        <span> {props.max} </span>
-        <span> {`Total price: ${total}`}</span>
-        <h1> {day.availaible} </h1>
+    <div className="booking__card_border">
+<div className="booking__card">
+    <div className="booking__date_guests">
+    <FaCalendarAlt className="booking__calendar_icon"/>
+        <h1 className="booking__day"> {day.day} </h1>
+        <FaDollarSign className="booking__dollar_sign"/>
+        <span className="booking__price_pp"> {price} per guest </span>
+        </div>
+        <div>
+        <FaUserPlus className="booking__user_icon"/>
         <input className="booking__input"
             type="number"
-            placeholder="Number of guests"
+            placeholder="Guests"
             value={guests < 15 ? guests : 15}
             onChange={(event) => { setGuests(event.target.value); console.log(guests, day.day) }}
         />
-        <button onClick={reserve}>Reserve</button>
+        <span className="booking__total_price"> <FaCalculator className="booking__calculator"/> {`Total: $${total}`}</span> 
+        </div>
+        <h1> {day.availaible} </h1>
+        <button onClick={reserve} className="booking__reserve_button">Reserve</button>
 {console.log(day.available,day.day, `DAY AND AVAILIBLE`)}
-    </>
-
+</div>
+</div>
 )
 })
 return (

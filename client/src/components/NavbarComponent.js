6326d42f36d28420f917757d8261bd75c8e4be07
mpham-12 <martinp0512@gmail.com>
2022-02-20 13:53:38 -0500

    initial changes to bootstrap navbar

--- client/src/components/NavbarComponent.js
+++ client/src/components/NavbarComponent.js
@@ -1,17 +1,18 @@
 import React from "react"
 import { Navbar, NavDropdown, Nav, Container } from 'react-bootstrap';
+import "./NavBarComponent.css";
 
 export default function NavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
+    <Navbar className="navbar" collapseOnSelect expand="lg" >
+      <Container className="navbar">
         <Navbar.Brand href="/">Daytrip</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/reservations">Reservations</Nav.Link>
-            <Nav.Link href="/favourites">Favourites</Nav.Link>
-            <Nav.Link href="/myreviews">Reviews</Nav.Link>
+            <Nav.Link className="nav-link" href="/reservations">Reservations</Nav.Link>
+            <Nav.Link className="nav-link" href="/favourites">Favourites</Nav.Link>
+            <Nav.Link className="nav-link" href="/myreviews">Reviews</Nav.Link>
             <NavDropdown title="My Adventures" id="collasible-nav-dropdown">
               <NavDropdown.Item href="/user/view">View</NavDropdown.Item>
               <NavDropdown.Item href="/user/create">Create</NavDropdown.Item>

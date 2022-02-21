91b1302daff45420637387c50fbea3c75f073977
mpham-12 <martinp0512@gmail.com>
2022-02-21 15:54:41 -0500

    style review form

--- client/src/components/ReviewForm.js
+++ client/src/components/ReviewForm.js
@@ -4,6 +4,11 @@ import useApplicationData from './../hooks/useApplicationData';
 import Typography from '@mui/material/Typography';
 import Rating from '@mui/material/Rating';
 import { userContext } from "../providers/UserProvider";
+import "./ReviewForm.css"
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import PublishIcon from '@mui/icons-material/Publish';
+
 
 export default function ReviewForm() {
   const { user } = useContext(userContext)
@@ -41,33 +46,61 @@ export default function ReviewForm() {
   return (
     <main className="review__form__page">
       <div className="Views">
-        <img src={views.thumbnail_photo_url} alt="" />
-        <img src={views.pic} alt=""/>
-        <img src={views.cover_photo_url} alt=""/>
-        <div>{views.first_name} {views.last_name}</div>
+        <div className="review-photod">
+        <img className="review-cover" src={views.cover_photo_url} alt="" />
+        <img className="review-thumbnail" src={views.thumbnail_photo_url} alt="" />
+        </div>
+
+        <div className="review-host-profile">
+
+        <img className="review-host-img" src={views.pic} alt="" />
+        <div className="review-host-name">
+          Hosted by: {views.first_name} {views.last_name}
+        <h2 className="header-leave-review">Leave the Host a Review</h2>
+          </div>
+        </div>
       </div>
       <div className="review">
         <div className="Star">
-          <Typography component="legend">Leave a Rating</Typography>
           <Rating
             name="simple-controlled"
             value={rating}
             onChange={(event, newRating) => {
               setRating(newRating);
             }}
-
+            style={{
+             fontSize: 80
+            }}
           />
         </div>
         <form onSubmit={event => event.preventDefault()}>
-          <input className="review__create-input"
-            type="text"
-            placeholder="Leave a review..."
+
+          <TextField
+            id="outlined-multiline-static"
+            label="Review"
+            multiline
+            rows={5}
             value={comment}
-            onChange={(event) => { setComment(event.target.value)}}
-          >
-          </input>
+            onChange={(event) => { setComment(event.target.value) }}
+            style={{ width: 500 }}
+          />
         </form>
-        <button onClick={review}>Submit</button>
+        <div className="submit-review-btn">
+
+        <Button
+          onClick={review}
+          variant="contained"
+          endIcon={<PublishIcon />}
+          style={{
+            backgroundColor: "#21b6ae",
+            width: 300,
+            height: 50,
+            fontSize: 18,
+            borderRadius: 35
+          }}
+        >Submit</Button>
+                </div>
+
       </div>
     </main >
   )

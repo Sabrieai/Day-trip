5d664af60a490199ec00e8b237a72ee67443f134
victhall <victoriahall.dev@gmail.com>
2022-02-20 21:09:34 -0500

    add video to homepage

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -30,16 +30,17 @@ export default function SearchBar() {
     <>
       {/* <label htmlFor="search">SEARCH</label> */}
       <form autoComplete="off" onSubmit={event => event.preventDefault()} className="search__form">
+        <div className="search__input_form">
         <input
           className="search-bar_input"
           type="text"
-          placeholder={`${placeholder} ... ?`}
+          placeholder={`${placeholder}...?`}
           value={search}
           onChange={(event) => setSearch(event.target.value)}
           data-testid="student-name-input"
         /> <FaMapMarkerAlt className="location__pin-icon"/>
         <button className="btn" onClick={() => navigate(`results/${search}`)} ><FaSearch className="search__icon"/></button>
-
+        </div>
       </form>
 
 

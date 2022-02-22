7795d6f2187ab37ea298dd50fb85528f70f16fe7
mpham-12 <martinp0512@gmail.com>
2022-02-21 20:48:36 -0500

    style reservations page

--- client/src/components/ReservationCard.js
+++ client/src/components/ReservationCard.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react"
 import useApplicationData from './../hooks/useApplicationData';
+import './ReservationCard.css';
+
 
 export default function ReservationCard(props) {
   return (
-    <section>
-      <img src={`${props.photo}`} alt="" />
-      <div>{props.title}</div>
-      <div>In {props.city}</div>
-      <div>On {props.date}</div>
+    <section className="res-card">
+      <div className="res-img-div">
+      <img className="res-img" src={`${props.photo}`} alt="" />
+      <div className="res-details">
+
+      <div className="res-title">{props.title}</div>
+      <div className="res-city">In {props.city}</div>
+      <div><span className="res-date">Reservation Date:</span> <span className="res-date-act">{props.date}</span></div>
+      </div>
+
+      </div>
     </section>
   )
 }

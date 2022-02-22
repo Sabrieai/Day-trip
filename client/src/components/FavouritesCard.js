dd161c18bd2a194c0bb3834fc0e121ff6b7d7382
mpham-12 <martinp0512@gmail.com>
2022-02-21 19:53:25 -0500

    fix favourites page

--- client/src/components/FavouritesCard.js
+++ client/src/components/FavouritesCard.js
@@ -8,13 +8,15 @@ const link = `/adventures/${props.id}`;
   return (
     <div className="center">
     <section className="favourites__card">
-      <a href={link}>
+      <a className="a-link" href={link}>
+        <div className="favourites-card-img">
         <img className="favourites__thumbnail" src={`${props.photo}`} alt="" />
+        </div>
         <div className="favourites__title">{props.title}</div>
-        <div>In {props.city}</div>
+        <div className="favourites__city">In {props.city}</div>
       </a>
       <div className="heart">
-      <Deleter  id={props.id} />
+      <Deleter id={props.id} />
       </div>
     </section>
     </div>

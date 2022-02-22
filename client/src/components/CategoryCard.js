02b6ccbdb1d862094a3b97413d30dd5580806f3f
mpham-12 <martinp0512@gmail.com>
2022-02-21 19:10:16 -0500

    style search results

--- client/src/components/CategoryCard.js
+++ client/src/components/CategoryCard.js
@@ -1,16 +1,26 @@
 import React, { useState, useEffect } from "react"
 import useApplicationData from '../hooks/useApplicationData';
+import "./CategoryCard.css"
 
 export default function CategoryCard(props) {
   const link = `/adventures/${props.id}`;
   console.log(props);
   //props.price undefined
   return (
-    <a href={link}>
-      <img src={props.photo} />
-      <div>{props.title}</div>
-      <div>In {props.city}</div>
-      <div>${props.price} per person</div>
+    <main className="results-container">
+
+    <div className="results-card">
+
+    <a className="review-card-link" href={link}>
+      <div className="results-card-img-div">
+      <img className="results-card-img" src={props.photo} />
+      </div>
+      <div className="adventure-card-title">{props.title}</div>
+      <div>in {props.city}</div>
+      <div className="adventure-card-price">${props.price}</div>
+      <span className="adventure-card-pp">per person</span>
     </a>
+    </div>
+    </main>
   )
 }
